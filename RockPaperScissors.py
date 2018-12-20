0d1ec17b40ccd7c5bb4f5c5aa2a4b589e962d22f
Shalom Vanderhoof <44070783+thesabermaniac@users.noreply.github.com>
2018-12-19 21:27:37 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,40 +1,47 @@
 import random
-cpu = random.randint(1, 3)
-user = input('Choose your weapon (Rock, Paper, or Scissors): ')
-while cpu == 1 and user.lower() == 'rock' \
-        or cpu == 2 and user.lower() == 'paper' \
-        or cpu == 3 and user.lower() == 'scissors':
-    print('You\'ve tied with the computer. Please try again.')
-    user = input('Choose your weapon (Rock, Paper, or Scissors): ')
 
 
 def main():
-    user_wins()
-    cpu_wins()
+    cpu = random.randint(1, 3)
+    user = input('Choose your weapon (Rock, Paper, or Scissors): ')
+    while tie_loop(user, cpu):
+        cpu = random.randint(1, 3)
+        user = input('Choose your weapon (Rock, Paper, or Scissors): ')
+    user_wins(user, cpu)
+    cpu_wins(user, cpu)
+    print('Thanks for playing. Good bye.')
 
 
-def user_wins():
-    if user.lower() == 'rock' and cpu == 3:
+def user_wins(player1, player2):
+    if player1.lower() == 'rock' and player2 == 3:
         print('The computer chose Scissors.')
         print('User wins!')
-    elif user.lower() == 'paper' and cpu == 1:
+    elif player1.lower() == 'paper' and player2 == 1:
         print('The computer chose Rock.')
         print('User wins!')
-    elif user.lower() == 'scissors' and cpu == 2:
+    elif player1.lower() == 'scissors' and player2 == 2:
         print('The computer chose Paper.')
         print('User wins!')
 
 
-def cpu_wins():
-    if user.lower() == 'rock' and cpu == 2:
+def cpu_wins(player1, player2):
+    if player1.lower() == 'rock' and player2 == 2:
         print('The computer chose Paper.')
         print('The computer wins :(')
-    elif user.lower() == 'paper' and cpu == 3:
+    elif player1.lower() == 'paper' and player2 == 3:
         print('The computer chose Scissors.')
         print('The computer wins :(')
-    elif user.lower() == 'scissors' and cpu == 1:
+    elif player1.lower() == 'scissors' and player2 == 1:
         print('The computer chose Rock.')
         print('The computer wins :(')
+
+
+def tie_loop(player1, player2):
+    if player2 == 1 and player1.lower() == 'rock' \
+            or player2 == 2 and player1.lower() == 'paper' \
+            or player2 == 3 and player1.lower() == 'scissors':
+        print('You\'ve tied with the computer. Please try again.')
+        return True
 
 
 main()
